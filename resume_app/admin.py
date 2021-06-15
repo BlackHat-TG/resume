40d6e6452d291eb1ae580409f7376319948a4aa2
BlackHat-TG <43718084+BlackHat-TG@users.noreply.github.com>
2021-06-15 18:27:00 +0530

    Delete resume_app directory

--- resume_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import rlogin
-from .models import basic_info
-from .models import personal
-
-admin.site.register(rlogin)
-admin.site.register(basic_info)
-admin.site.register(personal)
-
